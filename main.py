838d895905182149e5f9237e70a0502307370d23
QuentenB <quentenbaas@gmail.com>
2025-06-27 10:16:11 +0200

    deel 2 opdrqacht af, letters tellen lukt

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_letters
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -8,6 +8,9 @@ def get_book_text(filepath):
 def main():
     text = get_book_text("books/frankenstein.txt")
     aantalWoorden = get_num_words(text)
+    aantalLetters = get_num_letters(text)
     print(f"{aantalWoorden} words found in the document")
-
+    for letter, aantal in sorted(aantalLetters.items()):
+        print(f"'{letter}': {aantal}")
+    
 main()
--- stats.py
+++ stats.py
@@ -1,6 +1,19 @@
+import string
+
 def get_num_words(text):
     woordenLijst = text.split()
     teller = 0
     for i in range(0, len(woordenLijst), 1):
         teller += 1
     return teller
+
+def get_num_letters(text):
+    woorden_teller = {}
+    for character in text:
+        kleine_letter = character.lower()
+        if kleine_letter in woorden_teller:
+            woorden_teller[kleine_letter] += 1
+        else:
+            woorden_teller[kleine_letter] = 1
+    return woorden_teller
+
