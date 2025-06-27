00311a713ce8aa5860f164fbf6433d45138654d4
QuentenB <quentenbaas@gmail.com>
2025-06-27 09:27:16 +0200

    tel het aantal woorden

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ def get_book_text(filepath):
         text = f.read()
     return text
 
+def woorden_teller(text):
+    woordenLijst = text.split()
+    teller = 0
+    for i in range(0, len(woordenLijst), 1):
+        teller += 1
+    return teller
+
 def main():
     text = get_book_text("books/frankenstein.txt")
-    print(text)
+    woorden_teller(text)
     
 main()
