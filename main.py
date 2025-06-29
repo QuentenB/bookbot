087f6db82c8693c15c6d570153cba330c36d384f
QuentenB <quentenbaas@gmail.com>
2025-06-29 18:52:42 +0200

    elk boek nu als input

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words, get_num_letters, sorted_dic, sorteer_op
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -6,8 +7,11 @@ def get_book_text(filepath):
     return text
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
+    text = get_book_text(sys.argv[1])
     aantalWoorden = get_num_words(text)
     aantalLetters = get_num_letters(text)
 
